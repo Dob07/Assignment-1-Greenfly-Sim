f57aa22e9e3e9c99687992b4fb3beb0ba38beb95
Dobo <78497734+Dob07@users.noreply.github.com>
2022-11-02 21:31:18 +0000

    cba anymore

--- project1.py
+++ project1.py
@@ -22,15 +22,11 @@ juvenile = 0
 adult = 0
 senile = 0
 total = 0
-diseaseTrigger = 0
-diseaseDeathCount = 0
-
 genArray = []
 juvArray = []
 adArray = []
 senArray = []
 totalArray = []
-disArray = []
 titleArray = ['GENERATION', 'JUVENILES', 'ADULTS', 'SENILES', 'TOTAL']
 
 
@@ -38,8 +34,62 @@ genJuv = "       "
 juvAd = ""
 adSen = ""
 senTot = ""
-
 fullArray = []
+
+def clear():
+    global firstJuv          # Declare variables
+    global survivalJuv   
+    global firstAdults    
+    global survivalAdults
+    global firstSen     
+    global survivalSen       
+    global birthRate         
+    global noOfGens
+    global newJuv
+    global newAdults
+    global newSen
+    global juvenile 
+    global adult
+    global senile
+    global total
+    global genArray
+    global juvArray 
+    global adArray 
+    global senArray 
+    global totalArray 
+    global genJuv 
+    global juvAd 
+    global adSen 
+    global senTot
+    global fullArray
+    global titleArray
+    global br
+    firstJuv = 0
+    firstAdults = 0
+    firstSen = 0
+    survivalJuv = 0
+    survivalAdults = 0
+    survivalSen = 0
+    birthRate = 0 
+    noOfGens = 0
+    newJuv = 0 
+    newAdults = 0
+    newSen = 0
+    juvenile = 0
+    adult = 0
+    senile = 0
+    total = 0
+    genArray = []
+    juvArray = []
+    adArray = []
+    senArray = []
+    totalArray = []
+    titleArray = ['GENERATION', 'JUVENILES', 'ADULTS', 'SENILES', 'TOTAL']
+    genJuv = "       "
+    juvAd = ""
+    adSen = ""
+    senTot = ""
+    fullArray = []
 
 def firstGenVals():
     global firstJuv          # All of these globals are allowing the variables
@@ -52,8 +102,9 @@ def firstGenVals():
     global noOfGens  
     global juvenile 
     global adult
-    global senile   
-    global diseaseTrigger     
+    global senile      
+
+    clear()
 
     print("ALL THE VALUES ARE IN THOUSANDS")
 
@@ -79,14 +130,14 @@ def firstGenVals():
         survivalSen = float(input("Senile survival rate? (Between 0 and 1)\n"))
 
     birthRate = float(input("What is the birthRate?\n")) # Setting the birth rate
+    br = birthRate
 
     noOfGens = int(input("How many generations?\n")) # Setting how many generations the simulation will run for
     while noOfGens < 5 or noOfGens > 25: # This loop is making sure the number of generations is between 5 and 25
         print("Invalid generation range")
         noOfGens = int(input("How many generations?\n"))
 
-    diseaseTrigger = input("What should the population disease trigger point be? ")
-
+    diseaseTrigger = input("What should the population disease trigger point be? ") 
     menu() # return to menu
 
 def firstGenValShow(): # This subprogram is printing all the values that were put in
@@ -102,16 +153,35 @@ def firstGenValShow(): # This subprogram is printing all the values that were pu
     menu()
 
 def runSim(): # This runs the simulation 
-    global firstJuv, firstAdults, firstSen, survivalJuv, survivalAdults, survivalSen, birthRate, noOfGens, juvenile, adult, senile, diseaseTrigger, total, genArray, juvArray, adArray, senArray, totalArray, disArray, genJuv, juvAd, adSen, senTot, totDis, fullArray, jj, aa, ss, diseaseDeathCount
+    global firstJuv          # Declare variables
+    global survivalJuv       
+    global survivalAdults     
+    global survivalSen       
+    global birthRate         
+    global noOfGens
+    global newJuv
+    global newAdults
+    global newSen
+    global juvenile 
+    global adult
+    global senile
+    global total
+    global genArray
+    global juvArray 
+    global adArray 
+    global senArray 
+    global totalArray 
+    global genJuv 
+    global juvAd 
+    global adSen 
+    global senTot
+    global fullArray
+    global titleArray
+    global br
+
     noOfGens = noOfGens + 1
     gencheck = 0
     total = juvenile + adult + senile
-
-    flDis = float(diseaseTrigger)
-
-    newJuv = 0
-    newAdults = 0
-    newSen = 0
 
     for i in range(noOfGens): # This will run for the amount of generations that there are
         genArray.append(gencheck) # These append the values from 
@@ -119,54 +189,28 @@ def runSim(): # This runs the simulation
         adArray.append(adult)     # array depending on its type
         senArray.append(senile)
         totalArray.append(total)
-        disArray.append(diseaseDeathCount)
         tArr = [gencheck, juvenile, adult, senile, total] # This is an array for the file save
         fullArray.append(tArr) # This sends the tArr to the main array for the file save
+        br = birthRate
 
-        diseaseDeathCount = 0
+        newJuv = adult * br # This works out the new juveniles
+        newJuv = round(newJuv, 2) # This rounds it to 2 dp.
 
-        if total >= flDis:
-            deathPercent = random.randint(20,50)
-            deathPercent = float(deathPercent)
-            deathPercent = deathPercent / 100
+        newAdults = juvenile * survivalJuv # This works out the new adults
+        newAdults = round(newAdults, 2) 
 
-            jj = newJuv * deathPercent 
-            newJuv = newJuv - jj
-            newJuv = round(newJuv, 2) # This rounds it to 2 dp.
+        newSen = (senile * survivalSen) + (adult * survivalAdults) # This works out the new seniles
+        newSen = round(newSen, 2)
 
-            aa = newAdults * deathPercent
-            newAdults = newAdults - aa
-            newAdults = round(newAdults, 2) 
-
-            ss = newAdults * deathPercent
-            newSen = newSen - ss
-            newSen = round(newSen, 2)
-
-            total = newJuv + newAdults + newSen # This works out the total
-            total = round(total, 2)
-
-            diseaseDeathCount = jj + aa + ss
-            diseaseDeathCount = round(diseaseDeathCount, 2)
-
-        else:
-            newJuv = adult * birthRate # This works out the new juveniles
-            newJuv = round(newJuv, 2) # This rounds it to 2 dp.
-
-            newAdults = juvenile * survivalJuv # This works out the new adults
-            newAdults = round(newAdults, 2) 
-
-            newSen = (senile * survivalSen) + (adult * survivalAdults) # This works out the new seniles
-            newSen = round(newSen, 2)
-
-            total = newJuv + newAdults + newSen # This works out the total
-            total = round(total, 2)
+        total = newJuv + newAdults + newSen # This works out the total
+        total = round(total, 2)
 
         juvenile = newJuv # These set the new values as the old value and allow it to repeat
         adult = newAdults
         senile = newSen
         
         gencheck = gencheck + 1
-
+        
     print("GENERATION   JUVENILES   ADULTS   SENILES   TOTAL") # This is the top of the table
     for i in range(noOfGens):
         genJuv = "        "
@@ -179,34 +223,27 @@ def runSim(): # This runs the simulation
         sStr = str(senArray[i])
         if len(gStr) > 1:        #This is making sure the generation is in line with the top
             genJuv = "       "    
-
         a = len(jStr)            #This is making sure the juveniles is in line with the top
         while a != 10:
             juvAd = juvAd + " "
             a = a + 1
-
         a = len(aStr)            #This is making sure the adults is in line with the top
         while a != 7:
             adSen = adSen + " "
             a = a + 1
-
         a = len(sStr)            #This is making sure the senile is in line with the top
         while a != 8:
             senTot = senTot + " " 
             a = a + 1
-
         print("  ", genArray[i], genJuv, juvArray[i], juvAd, adArray[i], adSen, senArray[i], senTot, totalArray[i]) # This repeatedly prints to give the look of a table
         
     print(" ") 
     menu() # Takes back to menu
-
-
 def export(): # The export to file function
     global fullArray
     global noOfGens
     global titleArray
     ord = fullArray 
-
     exportedFile = input("What is the name of the file? ") # Asks for the name of the file
     if os.path.exists(exportedFile + ".csv"): # If the files name already exists then
         print("This file already exists")
@@ -242,8 +279,6 @@ def export(): # The export to file function
         f.close()
     menu()
             
-
-
     
 def menu(): # This subprogram is the menu where the program starts and returns to after a subprogram is done
     print("Greenfly Population\n")                          # These prints are showing the options that can be done
@@ -253,14 +288,12 @@ def menu(): # This subprogram is the menu where the program starts and returns t
     print("4: Save results")
     print("5: Quit")
     choiceSelect = int(input("\nPlease select the option 1, 2, 3, 4 or 5: ")) # Asking the user to input an option
-
     if choiceSelect != 1:                   # All of these if statements are checking that
         if choiceSelect != 2:               # they have picked a value that is valid
                 if choiceSelect != 3:
                     if choiceSelect != 4:
                         if choiceSelect != 5:
                             choiceSelect = int(input("\nPlease select the option 1, 2, 3, 4 or 5: ")) # If it isnt valid it asks the user again
-
     if choiceSelect == 1:
         firstGenVals() # This accesses the subprogram to change the values that will be used in the simuulation
     elif choiceSelect == 2:
@@ -271,5 +304,4 @@ def menu(): # This subprogram is the menu where the program starts and returns t
         export() # this will save the results of the simulation into a txt file
     elif choiceSelect == 5:
         exit() # this quits the program
-
 menu()
